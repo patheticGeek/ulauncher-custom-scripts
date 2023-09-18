bb0ae5ddf21098b190add5080fd80265f4c680cb
Pathetic Geek <geekpathetic@gmail.com>
2023-09-18 05:30:38 +0530

    yeeeeeee

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
+import subprocess
+import os
 import gi
 gi.require_version("Gdk", "3.0")
 
 from ulauncher.api.client.Extension import Extension
 from ulauncher.api.client.EventListener import EventListener
-from ulauncher.api.shared.event import KeywordQueryEvent
+from ulauncher.api.shared.event import KeywordQueryEvent, ItemEnterEvent
 from ulauncher.api.shared.action.RenderResultListAction import RenderResultListAction
+from ulauncher.api.shared.action.HideWindowAction import HideWindowAction
+from ulauncher.api.client.EventListener import EventListener
 
 from src.functions import strip_list
 from src.items import no_config_items, no_results_item, generate_launcher_items
@@ -16,6 +20,7 @@ class CustomSearchExtension(Extension):
         super(CustomSearchExtension, self).__init__()
 
         self.subscribe(KeywordQueryEvent, KeywordQueryEventListener())
+        self.subscribe(ItemEnterEvent, RunCommand())
 
 
 class KeywordQueryEventListener(EventListener):
@@ -35,6 +40,26 @@ class KeywordQueryEventListener(EventListener):
             return RenderResultListAction(no_results_item())
 
         return RenderResultListAction(generate_launcher_items(results, params))
+
+class RunCommand(EventListener):
+
+    def on_event(self, event, extension):
+
+        data = event.get_data()
+        
+        terminal = extension.preferences["term"]
+        exec = extension.preferences["exec"]
+        command = data["script"]
+        shell = extension.preferences["shell"]
+
+        envUserShell = os.environ["SHELL"] if data["exit_after"] != "true" else ""
+
+        if shell == "fish":
+            subprocess.run( [f'{terminal} {exec} {envUserShell} -c "{command}; {envUserShell}"'], shell=True )
+        else:
+            subprocess.run( [f'{terminal} {exec} {envUserShell} -c -i "{command}; {envUserShell}"'], shell=True )
+
+        return HideWindowAction()
 
 
 if __name__ == "__main__":
--- src/items.py
+++ src/items.py
@@ -1,10 +1,8 @@
 import os
 
 from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
-from ulauncher.api.shared.action.CopyToClipboardAction import CopyToClipboardAction
-from ulauncher.api.shared.action.HideWindowAction import HideWindowAction
 from ulauncher.api.shared.action.DoNothingAction import DoNothingAction
-from ulauncher.api.shared.action.RunScriptAction import RunScriptAction
+from ulauncher.api.shared.action.ExtensionCustomAction import ExtensionCustomAction
 
 from src.consts import ICON_FILE, SCRIPT_PATH
 
@@ -14,7 +12,7 @@ def no_config_items():
             icon=ICON_FILE,
             name='No scripts',
             description='Add scripts in ' + SCRIPT_PATH,
-            on_enter=RunScriptAction('xdg-open ' + SCRIPT_PATH)
+            on_enter=ExtensionCustomAction('xdg-open ' + SCRIPT_PATH)
         )
     ]
 
@@ -55,9 +53,8 @@ def generate_launcher_item(item, params):
         icon=get_icon(item.get('icon')),
         name=item.get('name', script) or script,
         description=description,
-        on_enter=RunScriptAction(script) if script else DoNothingAction()
+        on_enter=ExtensionCustomAction(script) if script else DoNothingAction()
     )
-
 
 def generate_launcher_items(results, params):
     return [
